cf22077181027db2f42daca2d910da15a32fd541
Jorge Salazar Cruz <cruzjorgesalazar@gmail.com>
2021-11-30 15:40:49 -0600

    easy circle case

--- src/gen_circle_points.py
+++ src/gen_circle_points.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-N = 70 #Numero de puntos en la circunferencia
+N = 10  # Numero de puntos en la circunferencia
 pi = np.pi
-r = 2 #radio de la circunferencia 
+r = 2  # radio de la circunferencia
 
 file = open("res/case4.in", "w")
-theta = np.linspace(0, 2*pi, num = N)
+theta = np.linspace(0, 2*pi, num=N)
 file.write(f'{N + 1}\n')
 
 for t in theta:
     file.write(f'{r*np.cos(t)} {r*np.sin(t)}\n')
 
-file.write(f'{0} {0}\n') #Agregar punto central
+file.write(f'{0} {0}\n')  # Agregar punto central
 file.close()
