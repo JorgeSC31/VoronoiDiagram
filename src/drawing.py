b20c7dfaf995a8a1f57169e763476fdbaece799a
ricGLo <71990554+ricGLo@users.noreply.github.com>
2021-11-29 22:48:47 -0600

    Cambio del formato de archivo de salida

--- src/drawing.py
+++ src/drawing.py
@@ -1,50 +1,51 @@
 from manim import *
-config.frame_width = 35
+config.frame_width = 15
 
 class Voronoi(Scene): 
     def construct(self):
-        file = open("test.txt","r")
+        file = open("case1.out ","r")
 
-        num_iterations = int(file.readline())
-        file.readline() #skip
+        num_iterations = int(file.readline()) #Numero de puntos del INPUT
 
-        for i in range(num_iterations):
+        for i in range(1, num_iterations+1):
             #Draw cells points
-            num_points = int(file.readline()) 
-            for j in range(num_points):
+            for j in range(i):
                 l = file.readline()
                 s = l.split(" ")
                 x = float(s[0])
                 y = float(s[1])
                 self.add(Dot([x, y, 0], color= PURE_RED, radius = 0.15))
 
-            file.readline() #skip
+            
+            l = file.readline()
+            s = l.split(" ")
+            x1 = float(s[0])
+            y1 = float(s[1])
+            x2 = float(s[2])
+            y2 = float(s[3])
+            while x1 < 10000:
+                dot1 = [x1, y1, 0]
+                dot2 = [x2, y2, 0]
 
+                #Add vertices
+                self.add(Dot(dot1, radius = 0.15))
+                self.add(Dot(dot2, radius = 0.15))
 
-            #Draw vertices
-            num_vertices = int(file.readline()) 
-            for j in range(num_vertices):
-                l = file.readline()
-                s = l.split(" ")
-                x = float(s[0])
-                y = float(s[1])
-                self.add(Dot([x, y, 0], radius = 0.15))
+                #Add half edges
+                self.add(Line(dot1, dot2))
 
-            file.readline() #skip
-
-            #Draw half edges
-            num_hedges = int(file.readline())
-            for j in range(num_hedges):
+                #Next line
                 l = file.readline()
                 s = l.split(" ")
                 x1 = float(s[0])
                 y1 = float(s[1])
                 x2 = float(s[2])
                 y2 = float(s[3])
-                self.add(Line([x1, y1, 0], [x2, y2, 0]))
             
-            file.readline() #skip
+            
             self.wait(1) #Wait 1 second
             self.clear() #Erase
 
         file.close()
+
+
