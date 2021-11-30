b6cbd65cd3768a6221ec0dbe7518ce181f879382
ricGLo <71990554+ricGLo@users.noreply.github.com>
2021-11-30 15:38:04 -0600

    Circle case generator of points

--- src/gen_circle_points.py
+++ src/gen_circle_points.py
@@ -6,7 +6,9 @@ r = 2 #radio de la circunferencia
 
 file = open("res/case4.in", "w")
 theta = np.linspace(0, 2*pi, num = N)
-file.write(f'{N + 1}\n')
+file.write(f'{N}\n')
+
+theta = theta[:-1] #Quitar theta = 2*pi para no repetir con theta = 0
 
 for t in theta:
     file.write(f'{r*np.cos(t)} {r*np.sin(t)}\n')
